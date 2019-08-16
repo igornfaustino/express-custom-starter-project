cb35d90324599cbedfa98e95fec67e3173d02dde
igornfaustino <igornfaustino@gmail.com>
2019-08-16 09:50:29 -0300

    fix lint style

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,26 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
+
 const fs = require('fs');
 const path = require('path');
 
 const basename = path.basename(module.filename);
 
-module.exports = (app) => {
+module.exports = app => {
 	// Load all js files in the folder and load routes
-	fs.readdirSync(__dirname).filter(file => {
-		// remove occult files and not js files
-		return (file.indexOf('.') != 0) && (file != basename) && (file.slice(-3) == '.js');
-	}).forEach(file => {
-		// require route and use on app
-		const routeFilePath = path.join(__dirname, file);
-		const route = require(routeFilePath);
-		app.use("/api/", route);
-	})
-}
+	fs.readdirSync(__dirname)
+		.filter(file => {
+			// remove occult files and not js files
+			return (
+				file.indexOf('.') !== 0 &&
+				file !== basename &&
+				file.slice(-3) === '.js'
+			);
+		})
+		.forEach(file => {
+			// require route and use on app
+			const routeFilePath = path.join(__dirname, file);
+			const route = require(routeFilePath);
+			app.use('/api/', route);
+		});
+};
